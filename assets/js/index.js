1495763e17f2a25fff51c5d00609a6674b3553b6
briantruong777 <briantruong777@gmail.com>
2013-09-20 00:39:13 -0500

    Optimizations are done
    There are still two different javascript files that share much in common
    and should be united.
    
    Also performance on Firefox is still lackluster

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,30 @@
 var LINK_FADE_TIME = 300;
 var BODY_FADE_TIME = 300;
 var ctx;
-var width;
-var height;
-var ctrX;
-var ctrY;
+var scrWidth;
+var scrHeight;
 var mouseX;
 var mouseY;
-var pts = new Array();
-var timeout;
+var pts = []; // [x,y,xVelocity,yVelocity, ... ]
+var genTimer;
 
+// Called when window resized
 function resized()
 {
-	width=$(window).width();
-	height=$(window).height();
-	ctrX = Math.floor(width / 2);
-	ctrY = Math.floor(height / 2);
-	$("#bg_canvas").attr("width",width);
-	$("#bg_canvas").attr("height",height);
+	scrWidth=window.innerWidth;
+	scrHeight=window.innerHeight;
+	$("#bg_canvas").attr("width",scrWidth);
+	$("#bg_canvas").attr("height",scrHeight);
 }
 
+// Called when mouse is moved over Canvas
 function getMousePos(event)
 {
 	mouseX = event.clientX;
 	mouseY = event.clientY;
 }
 
+// Adds new random point to array
 function genPoint()
 {
 	var velX = Math.floor(21*Math.random()) - 10;
@@ -46,42 +45,40 @@ function genPoint()
 	pts.push(mouseX, mouseY, velX, velY);
 }
 
+// Generates a point and then sets up timer to generate more points
 function genPointCont(event)
 {
 	genPoint();
-	timeout = setInterval(genPoint, 10);
+	genTimer = setInterval(genPoint, 10);
 }
 
+// Cancels the point generator timer
 function cancelGenPointCont()
 {
-	clearInterval(timeout);
+	clearInterval(genTimer);
 }
 
+// Sets a specific pixel white
 function setPixel(x, y, pImageData)
 {
-	var idx = 4*(width*y + x);
-	pImageData.data[idx+0] = 255;
+	var idx = 4*(scrWidth*y + x);
+	pImageData.data[idx] = 255;
 	pImageData.data[idx+1] = 255;
 	pImageData.data[idx+2] = 255;
 	pImageData.data[idx+3] = 255;
 }
 
-function clearPixel(x, y, pImageData)
-{
-	var idx = 4*(width*y + x);
-	pImageData.data[idx+0] = 0;
-	pImageData.data[idx+1] = 0;
-	pImageData.data[idx+2] = 0;
-	pImageData.data[idx+3] = 0;
-}
-
+// Draws a single animation step. Moves all points.
 function draw(timeStamp)
 {
-	var imageData = ctx.createImageData(width, height);
-	for (var i = 0; i < pts.length; i += 4)
+	var imageData = ctx.createImageData(scrWidth, scrHeight);
+  var x;
+  var y;
+  var len = pts.length;
+	for (var i = 0; i < len; i += 4)
 	{
-		var x = pts[i];
-		var y = pts[i+1];
+		x = pts[i];
+		y = pts[i+1];
 
 		x += pts[i+2];
 		y += pts[i+3];
@@ -91,9 +88,9 @@ function draw(timeStamp)
 			x = 0;
 			pts[i+2] = -1 * pts[i+2];
 		}
-		else if (x > width)
+		else if (x > scrWidth)
 		{
-			x = width;
+			x = scrWidth;
 			pts[i+2] = -1 * pts[i+2];
 		}
 
@@ -102,9 +99,9 @@ function draw(timeStamp)
 			y = 0;
 			pts[i+3] = -1 * pts[i+3];
 		}
-		else if (y > height)
+		else if (y > scrHeight)
 		{
-			y = height;
+			y = scrHeight;
 			pts[i+3] = -1 * pts[i+3];
 		}
 
@@ -118,86 +115,60 @@ function draw(timeStamp)
 	window.requestAnimationFrame(draw);
 }
 
-// Adds event handlers and starts some animations
+// Code specifically for Firefox's bfcaching causing issues:
+// This is run whenever Firefox goes back/forwards and a cached page is
+// brought to the front
+// Also run on jQuery's ready()
 function onPageShow()
 {
-	// Following two lines for browsers that bfcache, thanks Firefox
-	$(".navbar-link").css("opacity","0.3");
-	$("#navbar-current").css("opacity","1");
-
 	// Fade in main part of <body>
 	$("#body-center").css("display", "none");
 	$("#body-center").fadeIn(BODY_FADE_TIME);
 
+  // Make sure normal css styles are restored
+	$(".navbar-link").css("opacity","0.3");
+	$("#navbar-current").css("opacity","1");
+
+  // Fade in canvas
 	$("#bg_canvas").css("display", "none");
 	$("#bg_canvas").fadeIn(BODY_FADE_TIME);
-
-
 }
+
+// For link hover event listener
+function aniLinkOn()
+{
+  $(this).animate({opacity:'1'}, {duration:LINK_FADE_TIME,queue:false});
+}
+// For link hover event listener
+function aniLinkOff()
+{
+  $(this).animate({opacity:'0.3'}, {duration:LINK_FADE_TIME,queue:false});
+}
+
+function linkClick(event)
+{
+  // Intercept page change
+  event.preventDefault();
+  // Don't allow link to go dark
+  $(this).unbind("mouseleave");
+  // Save destination page
+  var dest = this.href;
+  // Fade out current page's link
+  $("#navbar-current").animate({opacity:'0.3'},{duration:LINK_FADE_TIME,queue:false});
+  // Fade out the canvas
+  $("#bg_canvas").fadeOut(BODY_FADE_TIME);
+  // Fade out body and only when done, load next page
+  $("#body-center").fadeOut(BODY_FADE_TIME,
+    function(){window.location.assign(dest);}
+  );
+}
+
+
+
 
 $(document).ready(function()
 {
-	$("#navbar").css("background-color","transparent");
-
-	resized();
-	window.onresize = resized;
-
-	var c = document.getElementById('bg_canvas');
-	ctx=c.getContext('2d');
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(1);
-	pts.push(0);
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(0);
-	pts.push(1);
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(-1);
-	pts.push(0);
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(0);
-	pts.push(-1);
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(1);
-	pts.push(1);
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(-1);
-	pts.push(1);
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(1);
-	pts.push(-1);
-	pts.push(ctrX);
-	pts.push(ctrY);
-	pts.push(-1);
-	pts.push(-1);
-
-	c.onmousedown = genPointCont;
-	c.onmouseup = cancelGenPointCont;
-	c.onmouseout = cancelGenPointCont;
-	c.onmousemove = getMousePos;
-
-//	window.setInterval(draw,10);
-	window.requestAnimationFrame(draw);
-
-	// Hover effect for links
-	$(".navbar-link").hover(
-		function()
-		{
-			$(this).animate({opacity:'1'}, {duration:LINK_FADE_TIME,queue:false});
-		},
-		function()
-		{
-			$(this).animate({opacity:'0.3'}, {duration:LINK_FADE_TIME,queue:false});
-		});
-	$("#navbar-current").unbind("mouseleave");
-	// Firefox's bfcaching causes issues unless I use onpageshow event
-	// instead of relying on jQuery's ready()
+  // If Chrome of Firefox
 	if (window.onpageshow === null)
 	{
 		// Chrome will also take this path
@@ -209,17 +180,29 @@ $(document).ready(function()
 		onPageShow();
 	}
 
+	// Hover effect for links
+	$(".navbar-link").hover(aniLinkOn, aniLinkOff);
+  // Don't do effect on current link
+	$("#navbar-current").unbind("mouseleave");
+
 	// Page fade out occurs whenever any navbar-link is clicked
-	$(".navbar-link").click(function(event) {
-		event.preventDefault(); // Intercept page change
-		$(this).unbind("mouseleave"); // Don't allow link to go dark
-		var dest = this.href; // Save destination page
+	$(".navbar-link").click(linkClick);
 
-		$("#bg_canvas").fadeOut(BODY_FADE_TIME);
 
-		// Fade out current page's link
-		$("#navbar-current").animate({opacity:'0.3'},{duration:LINK_FADE_TIME,queue:false});
-		// Fade out body and only when done, load next page
-		$("#body-center").fadeOut(BODY_FADE_TIME,function(){window.location.assign(dest);});
-	});
+
+  // Fill in the initial values for window size
+	resized();
+  // Set up event listener
+	window.onresize = resized;
+
+	var c = document.getElementById('bg_canvas');
+	ctx=c.getContext('2d');
+
+	c.onmousedown = genPointCont;
+	c.onmouseup = cancelGenPointCont;
+	c.onmouseout = cancelGenPointCont;
+	c.onmousemove = getMousePos;
+
+  // Begins animation cycle
+	window.requestAnimationFrame(draw);
 });
--- assets/js/website.js
+++ assets/js/website.js
@@ -1,54 +1,66 @@
 var LINK_FADE_TIME = 300;
 var BODY_FADE_TIME = 300;
 
-// Code specifically for Firefox's bfcaching causing issues
+// Code specifically for Firefox's bfcaching causing issues:
+// This is run whenever Firefox goes back/forwards and a cached page is
+// brought to the front
+// Also run on jQuery's ready()
 function onPageShow()
 {
   // Fade in main part of <body>
-  var body_center = $("#body-center");
-  body_center.css("display", "none");
-  body_center.fadeIn(BODY_FADE_TIME);
+  $("#body-center").css("display", "none");
+  $("#body-center").fadeIn(BODY_FADE_TIME);
 
-  // Following two lines for browsers that bfcache, thanks Firefox
+  // Make sure normal css styles are restored
   $(".navbar-link").css("opacity","0.3");
   $("#navbar-current").css("opacity","1");
 }
 
+// For link hover event listener
+function aniLinkOn()
+{
+  $(this).animate({opacity:'1'}, {duration:LINK_FADE_TIME,queue:false});
+}
+// For link hover event listener
+function aniLinkOff()
+{
+  $(this).animate({opacity:'0.3'}, {duration:LINK_FADE_TIME,queue:false});
+}
+
+// For link click event listener
+function linkClick(event)
+{
+  // Intercept page change
+  event.preventDefault();
+  // Don't allow link to go dark
+  $(this).unbind("mouseleave");
+  // Save destination page
+  var dest = this.href;
+  // Fade out current page's link
+  $("#navbar-current").animate({opacity:'0.3'},{duration:LINK_FADE_TIME,queue:false});
+  // Fade out body and only when done, load next page
+  $("#body-center").fadeOut(BODY_FADE_TIME,
+    function(){window.location.assign(dest);}
+  );
+}
+
 $(document).ready(function()
 {
-  // Firefox's bfcaching causes issues unless I use onpageshow event
-  // rather than relying on jQuery's ready()
+  // If Chrome or Firefox
   if (window.onpageshow === null)
   {
-    // Chrome will also take this path
     window.addEventListener("pageshow", onPageShow, false);
   }
-  else
+  else // IE
   {
-    // If browser doesn't support onpageshow, do it now
     onPageShow();
   }
 
-  navbar_link = $(".navbar-link");
-  navbar_current = $("#navbar-current");
-
   // Hover effect for links
-  navbar_link.hover(
-    function() {
-      $(this).animate({opacity:'1'}, {duration:LINK_FADE_TIME,queue:false});
-    }, function() {
-      $(this).animate({opacity:'0.3'}, {duration:LINK_FADE_TIME,queue:false});
-    });
-  navbar_current.unbind("mouseleave"); // Don't do effect on current link
+  $(".navbar-link").hover(aniLinkOn, aniLinkOff);
+  // Don't do effect on current link
+  $("#navbar-current").unbind("mouseleave");
 
   // Page fade out occurs whenever any navbar-link is clicked
-  navbar_link.click(function(event) {
-    event.preventDefault(); // Intercept page change
-    $(this).unbind("mouseleave"); // Don't allow link to go dark
-    var dest = this.href; // Save destination page
-    // Fade out current page's link
-    navbar_current.animate({opacity:'0.3'},{duration:LINK_FADE_TIME,queue:false});
-    // Fade out body and only when done, load next page
-    $("#body-center").fadeOut(BODY_FADE_TIME,function(){window.location.assign(dest);});
-  });
+  $(".navbar-link").click(linkClick);
 });
